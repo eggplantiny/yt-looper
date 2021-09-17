084a08da5ef6260878bc5097eea4684d1359bc56
eggplantiny <eggplantiny@gmail.com>
2021-09-17 23:39:28 +0900

    Home 화면 수정

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,43 @@
 <template>
   <div>
+    <text-field
+      v-model="searchValue"
+      @keydown.enter="onEntered"
+    />
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import TextField from '@/components/atoms/TextField.vue'
+import {
+  ref
+} from 'vue'
+
+import {
+  useRoute,
+  useRouter
+} from "vue-router"
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    TextField
+  },
+  setup () {
+    const searchValue = ref('')
+    const route = useRoute()
+    const router = useRouter()
+
+    const onEntered = () => {
+      const searchTarget = searchValue.value
+      router.push(`/${searchTarget}`)
+    }
+
+    return {
+      searchValue,
+      onEntered
+    }
+  }
 }
 </script>
 
