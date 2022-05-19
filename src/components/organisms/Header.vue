a171a75e3669f2848bb9f3b1afa3a39be9153a30
eggplantiny <eggplantiny@gmail.com>
2022-05-19 18:12:15 +0900

    feat > use DaisyUI Header and modify folder structure

---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navbar bg-base-100">
+    <a class="btn btn-ghost normal-case text-xl">
+      Youtube Looper
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+</script>
+
+<style scoped>
+
+</style>
